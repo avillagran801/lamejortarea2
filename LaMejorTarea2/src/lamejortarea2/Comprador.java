277dcb7c8eab61b91fa58d6233d2f7c89d04d778
Ana <anvillagran2021@inf.udec.cl>
2022-10-17 21:32:51 -0300

    Bug al entregar vuelto por monto menor al precio

--- LaMejorTarea2/src/lamejortarea2/Comprador.java
+++ LaMejorTarea2/src/lamejortarea2/Comprador.java
@@ -6,14 +6,31 @@ class Comprador {
     
     public Comprador(int idBebida, Moneda moneda, Expendedor expendedor){
         vuelto = 0;
-        sabor = expendedor.comprarBebida(idBebida, moneda).beber();
-        System.out.println(sabor);
         
-        while(expendedor.getVuelto() != null){
-            vuelto = vuelto + 100;
+        expendedor.comprarBebida(idBebida, moneda);
+        
+        // AQUÍ HAY QUE HACER ALGO EN CASO DE QUE SALGA NULL EN LA COMPRA
+        // PARA QUE SALGA ESE ERROR Y NO EL DE NullPointerException        
+        
+        // sabor = expendedor.comprarBebida(idBebida, moneda).beber();
+        // System.out.println(sabor);
+        
+        // Esto hace que el vuelto termine de entregarse ya sea cuando se
+        // entregue la misma moneda o cuando terminen las monedas de $100 para
+        // la diferencia
+        
+        Boolean vuelto_fin = false;
+        
+        while(!vuelto_fin){
+            Moneda vuelto_aux = expendedor.getVuelto();
+            if(vuelto_aux != null){
+                vuelto = vuelto + vuelto_aux.getValor();
+            }
+            else {
+                vuelto_fin = true;
+            }
         }
 
-        
-        System.out.println("Precio bebida: $" + expendedor.getPrecio() + "\nMonto abonado: $" + moneda.getValor() + "\nVuelto: $" + vuelto);
+        System.out.println("Precio bebida: $" + expendedor.getPrecio() + "\nMonto abonado: $" + moneda.getValor() + "\nDevolución: $" + vuelto);
     }
 }
